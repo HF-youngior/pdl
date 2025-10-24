3fafa705c552f32eebcc56a9928def9c78613ac5
ShiYi-0929 <23301079@bjtu.edu.cn>
2025-10-24 13:53:17 +0800

    ai地图环境配置

--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/task.dart';
+import 'api_service.dart';
 
 class TaskService {
-  static const String baseUrl = 'http://10.0.2.2:8080/api';
+  static String get baseUrl => ApiService.baseUrl;
   static String? _authToken;
   
   // 设置认证token
